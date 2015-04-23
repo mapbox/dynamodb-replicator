96cda5846fde3bfecca684478fa379c6414befe4
Ryan Clark <ryan.clark.j@gmail.com>
2015-04-22 18:09:27 -0700

    sketch standalone backup scripts

---
+++ backup.js
@@ -0,0 +1,79 @@
+var AWS = require('aws-sdk');
+var s3Stream = require('s3-upload-stream')(new AWS.S3());
+var queue = require('queue-async');
+var Dyno = require('dyno');
+var stream = require('stream');
+
+module.exports = function(config, done) {
+    var primary = Dyno(config);
+    var throughput = require('dynamodb-throughput')(config.table, { region: config.region });
+
+    var log = config.log || console.log;
+    var scanOpts = config.hasOwnProperty('segment') && config.segments ?
+        { segment: config.segment, segments: config.segments } : undefined;
+
+    if (config.backup)
+        if (!config.backup.bucket || !config.backup.prefix || !config.backup.jobid)
+            return done(new Error('Must provide a bucket, prefix and jobid for backups'));
+
+    var index = !isNaN(parseInt(config.segment)) ? config.segment.toString() : 0;
+    var key = [config.backup.prefix, config.backup.jobid, index].join('/');
+
+    var writeBackup = new stream.Transform({ objectMode: true });
+
+    writeBackup.upload = s3Stream.upload({
+        Bucket: config.backup.bucket,
+        Key: key
+    }).on('error', function(err) {
+        writeBackup.emit('error', err);
+    });
+
+    writeBackup.count = 0;
+
+    writeBackup._transform = function(record, enc, callback) {
+        var line = JSON.stringify(record, function(key) {
+            var value = this[key];
+            if (Buffer.isBuffer(value)) return 'base64:' + value.toString('base64');
+            return value;
+        });
+
+        writeBackup.upload.write(line + '\n');
+        this.push(record);
+        writeBackup.count++;
+        callback();
+    };
+
+    writeBackup._flush = function(callback) {
+        writeBackup.upload.on('uploaded', function(uploadInfo) {
+            log('[segment %s] Uploaded dynamo backup to s3://%s/%s', index, config.backup.bucket, key);
+            log('[segment %s] Wrote %s items to backup', index, writeBackup.count);
+            callback();
+        });
+        writeBackup.upload.end();
+    };
+
+    var report;
+
+    queue(1)
+        .defer(throughput.setCapacity, { read: 1000 })
+        .defer(function(next) {
+            report = setInterval(function() {
+                log('[segment %s] Wrote %s items to backup', index, writeBackup.count);
+            }, 1 * 1000);
+
+            primary.scan(scanOpts)
+                .on('error', next)
+                .pipe(writeBackup)
+                .on('error', next)
+                .on('finish', next);
+        })
+        .awaitAll(function(backupErr) {
+            clearInterval(report);
+
+            throughput.resetCapacity(function(resetErr) {
+                if (backupErr) return done(backupErr);
+                if (resetErr) return done(resetErr);
+                done();
+            });
+        });
+};
