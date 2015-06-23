3ebee99454e7a1f1d2c76c347fc9accfea62ad0e
Ryan Clark <ryan.clark.j@gmail.com>
2015-06-23 11:41:30 -0600

    adjust lambda for dynamodb streams instead of kinesis-hack

--- index.js
+++ index.js
@@ -8,56 +8,49 @@ module.exports.streambot = streambot(replicate);
 function replicate(event, callback) {
     console.log('Env: %s', JSON.stringify(process.env));
 
-    var primaryConfig = { region: process.env.PrimaryRegion };
-    if (process.env.PrimaryEndpoint) primaryConfig.endpoint = process.env.PrimaryEndpoint;
-    var primary = new AWS.DynamoDB(primaryConfig);
-
     var replicaConfig = { region: process.env.ReplicaRegion };
     if (process.env.ReplicaEndpoint) replicaConfig.endpoint = process.env.ReplicaEndpoint;
     var replica = new AWS.DynamoDB(replicaConfig);
 
+    var allRecords = event.Records.reduce(function(allRecords, action) {
+        var id = JSON.stringify(action.Dynamodb.Keys);
+
+        allRecords[id] = allRecords[id] || [];
+        allRecords[id].push(action);
+        return allRecords;
+    }, {});
+
     var q = queue();
 
-    event.Records
-        .filter(function(record) {
-            return record.eventName === 'aws:kinesis:record';
-        })
-        .forEach(function(record) {
-            record.kinesis.data = new Buffer(record.kinesis.data, 'base64').toString();
-            record = record.kinesis;
-
-            q.defer(function(next) {
-                var data;
-                try { data = JSON.parse(record.data); }
-                catch (err) { return next(err); }
-
-                var getParams = {
-                    TableName: process.env.PrimaryTable,
-                    Key: data.dynamodb.Keys,
-                    ConsistentRead: true
-                };
-
-                console.log('Processing: %s', JSON.stringify(getParams.Key));
-                console.log('GET: %s', JSON.stringify(getParams));
-
-                primary.getItem(getParams, function(err, response) {
-                    if (err) return next(err);
-
-                    if (!response.Item) return replica.deleteItem({
-                        TableName: process.env.ReplicaTable,
-                        Key: data.dynamodb.Keys
-                    }, next);
-
-                    var putParams = {
-                        TableName: process.env.ReplicaTable,
-                        Item: response.Item
-                    };
-
-                    console.log('PUT: %s', JSON.stringify(putParams));
-                    replica.putItem(putParams, next);
-                });
-            });
+    Object.keys(allRecords).forEach(function(key) {
+        q.defer(processRecord, allRecords[key], replica);
     });
 
     q.awaitAll(callback);
+}
+
+ function processRecord(changes, replica, callback) {
+    var thisRecord = queue(1);
+
+    changes.forEach(function(change) {
+        thisRecord.defer(processChange, change, replica);
+    });
+
+    thisRecord.awaitAll(callback);
+}
+
+function processChange(record, replica, callback) {
+    console.log('Processing %s to %j', record.EventName, record.Dynamodb.Keys);
+
+    if (record.EventName === 'INSERT' || record.EventName === 'MODIFY') {
+        replica.putItem({
+            TableName: process.env.ReplicaTable,
+            Item: record.Dynamodb.NewImage
+        }, callback);
+    } else if (record.EventName === 'REMOVE') {
+        replica.deleteItem({
+            TableName: process.env.ReplicaTable,
+            Key: record.Dynamodb.Keys
+        }, callback);
+    }
 }
