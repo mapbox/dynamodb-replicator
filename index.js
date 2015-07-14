1ef31a1744e1ed98766170b62169005c9efd7f0f
jakepruitt <jake@mapbox.com>
2015-07-14 14:11:46 -0400

    Possible timestamp implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var AWS = require('aws-sdk');
 var queue = require('queue-async');
+var crypto = require('crypto');
 var streambot = require('streambot');
 
 module.exports = replicate;
@@ -8,7 +9,23 @@ module.exports.streambot = streambot(replicate);
 function replicate(event, callback) {
     console.log('Env: %s', JSON.stringify(process.env));
 
-    var replicaConfig = { region: process.env.ReplicaRegion };
+    if (change.eventSourceARN.indexOf(process.env.PrimaryTables[0]) > -1) {
+        var targetTable = process.env.PrimaryTables[1];
+        var targetRegion = process.env.PrimaryRegions[1];
+        var sourceTable = process.env.PrimaryTables[0];
+        var sourceRegion = process.env.PrimaryRegions[0];
+    } else {
+        var targetTable = process.env.PrimaryTables[0];
+        var targetRegion = process.env.PrimaryRegions[0];
+        var sourceTable = process.env.PrimaryTables[1];
+        var sourceRegion = process.env.PrimaryRegions[1];
+    }
+    console.log('source table: %s', sourceTable);
+    console.log('target table: %s', targetTable);
+
+    var targetConfig = { region: process.env.ReplicaRegion };
+    var sourceConfig = { region: process.env.ReplicaRegion };
+
     if (process.env.ReplicaEndpoint) replicaConfig.endpoint = process.env.ReplicaEndpoint;
     var replica = new AWS.DynamoDB(replicaConfig);
     console.log(replicaConfig);
@@ -32,14 +49,7 @@ function replicate(event, callback) {
 }
 
 function processChange(change, replica, callback) {
-    var tableName;
     console.log('Processing %s to %j', change.EventName, change.Dynamodb.Keys);
-    if (change.eventSourceARN.indexOf(process.env.PrimaryTables[0]) > -1) {
-        tableName = process.env.PrimaryTables[0];
-    } else {
-        tableName = process.env.PrimaryTables[1];
-    }
-    console.log('putting to ' + tableName);
 
     var conditionalPutParams = {
         TableName: tableName,
@@ -77,20 +87,46 @@ function processChange(change, replica, callback) {
             dynamo.getItem({
                 TableName: tableName,
                 Key: change.Dynamodb.Keys
-            }, function(err, item) {
+            }, function(err, targetItem) {
                 if (err) return callback(err);
 
-                if (!itemsEqual(item, change.Dynamodb.NewImage)) {
-                    // Put the newest item if the records differ, without any conditional checks
-                    console.log('Item in conflict');
-                    return dynamo.putItem({
-                        TableName: tableName,
-                        Item: change.Dynamodb.NewImage
-                    }, callback);
-                } else {
+                if (itemsEqual(targetItem, change.Dynamodb.NewImage)) {
                     // Item has already been put, no need to update
                     console.log('Item already in correct state');
                     return callback();
+                } else {
+                    // If the records differ, compare modified, and resolve to the most recent
+                    console.log('Item in conflict');
+                    if (targetItem.modified > change.Dynamodb.NewImage.modified) {
+                        // Existing item is newer, write it to the source database
+                        return dynamo.putItem({
+                            TableName: sourceTable,
+                            Item: targetItem
+                        }, callback);
+                    } else if (targetItem.modified < change.Dynamodb.NewImage.modified) {
+                        // Update from source is newer, write to the target database
+                        return dynamo.putItem({
+                            TableName: targetTable,
+                            Item: change.Dynamodb.NewImage
+                        }, callback);
+                    } else { // modified is equal, resolve with the record with the alphabetically first md5 hash
+                        var md5target = crypto.createHash('md5');
+                        md5target.update(JSON.stringify(targetItem), 'utf8');
+                        var targetHash = md5target.digest('hex');
+                        var md5source = crypto.createHash('md5');
+                        md5source.update(JSON.stringify(change.Dynamodb.NewImage), 'utf8');
+                        var source = md5source.digest('hex');
+                        if (targetHash > sourceHash) {
+                            return dynamo.putItem({
+                                TableName: sourceTable,
+                                Item: targetItem
+                            }, callback);
+                        } else {
+                            return dynamo.putItem({
+                                TableName: targetTable,
+                                Item: change.Dynamodb.NewImage
+                            }, callback);
+                        }
                 }
             });
         } else if (err) {
